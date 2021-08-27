e727ad902bf7a08e6a616db3b3302926001c6dd0
katskurka <kati.skurka@gmail.com>
2021-08-27 00:35:07 -0400

    updated sql, added models folder and files

--- villains.sql
+++ villains.sql
@@ -1,5 +1,11 @@
 CREATE DATABASE disney_villains;
 
+DROP USER IF EXISTS 'disneyVillains'@'localhost';
+CREATE USER 'disneyVillains'@'localhost' IDENTIFIED BY 'fOoL$!';
+
+GRANT ALL PRIVILEGES ON disneyVillians.* to 'disneyVillains'@'localhost';
+FLUSH PRIVILEGES;
+
 USE disney_villains;
 
 CREATE TABLE villains (
@@ -14,43 +20,23 @@ CREATE TABLE villains (
 
 INSERT INTO villains (name, movie, slug)
 VALUES ('Captain Hook', 'Peter Pan', 'captain-hook'),
-  (
-    'Cruella de Vil',
-    'One Hundred and One Dalmatians',
-    'cruella-de-vil'
-  ),
+  ('Cruella de Vil', 'One Hundred and One Dalmatians', 'cruella-de-vil'),
   ('Gaston', 'Beauty and the Beast', 'gaston'),
   ('Hades', 'Hercules', 'hades'),
-  (
-    'Horned King',
-    'The Black Cauldron',
-    'horned-king'
-  ),
+  ('Horned King', 'The Black Cauldron', 'horned-king'),
   ('Jafar', 'Aladdin', 'jafar'),
   ('Lady Tremaine', 'Cinderella', 'lady-tremaine'),
   ('Madame Medusa', 'The Rescuers', 'madame-medusa'),
-  (
-    'Madam Mim',
-    'The Sword in the Stone',
-    'madam-mim'
-  ),
+  ('Madam Mim', 'The Sword in the Stone', 'madam-mim'),
   ('Maleficent', 'Sleeping Beauty', 'maleficent'),
   ('Prince John', 'Robin Hood', 'prince-john'),
   ('Sir Hiss', 'Robin Hood', 'sir-hiss'),
-  (
-    'Queen Grimhilde',
-    'Snow White and the Seven Dwarfs',
-    'queen-grimhilde'
-  ),
-  (
-    'Queen of Hearts',
-    'Alice in Wonderland',
-    'queen-of-hearts'
-  ),
+  ('Queen Grimhilde', 'Snow White and the Seven Dwarfs', 'queen-grimhilde'),
+  ('Queen of Hearts', 'Alice in Wonderland', 'queen-of-hearts'),
   ('Scar', 'The Lion King', 'scar'),
   ('Shan Yu', 'Mulan', 'shan-yu'),
   ('Shere Khan', 'The Jungle Book', 'shere-khan'),
   ('Ursula', 'The Little Mermaid', 'ursula');
-  
+
 SELECT *
 FROM villains;
