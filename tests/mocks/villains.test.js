8baecee61969536fa263f1c8be6f81aa61ae205a
katskurka <kati.skurka@gmail.com>
2021-09-02 13:40:53 -0400

    added mock data to mock file

---
+++ tests/mocks/villains.test.js
@@ -0,0 +1,21 @@
+const everyVillain = [{
+  name: 'Hades',
+  movie: 'Hercules',
+  slug: 'hades',
+}, {
+  name: 'Horned King',
+  movie: 'The Black Cauldron',
+  slug: 'horned-king',
+}, {
+  name: 'Jafar',
+  movie: 'Aladdin',
+  slug: 'jafar',
+}]
+
+const oneVillain = {
+  name: 'Maleficent',
+  movie: 'Sleeping Beauty',
+  slug: 'maleficent',
+}
+
+module.exports = { everyVillain, oneVillain }
